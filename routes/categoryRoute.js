0636fa484375cf1ee56e49c5ed513c962ae2c93e
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-02-16 23:09:52 +0530

    api create for product

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,14 +2,61 @@ const express = require("express");
 const { Category } = require("../models/categorySchema");
 const router = express.Router();
 
+//router for all get category
 router.get("/getcategory", async (req, res) => {
 	const categoryList = await Category.find();
 	if (!categoryList) {
 		return res.status(500).json({ succes: false });
 	}
-	res.send(categoryList);
+	res.status(200).send(categoryList);
 });
 
+//router for get category by id
+router.get("/getcategory/:id", async (req, res) => {
+	try {
+		const category = await Category.findById(req.params.id);
+		if (!category) {
+			return res
+				.status(404)
+				.json({ message: "category with the given id is not found" });
+		}
+		res.status(200).json({ category });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+//router for update the category
+router.put("/updatecategory/:id", async (req, res) => {
+	try {
+		if (!req.body.name) {
+			return res
+				.status(400)
+				.json({ success: false, message: "Category name is required" });
+		}
+		const category = await Category.findByIdAndUpdate(
+			req.params.id, // Get ID from URL
+			{
+				name: req.body.name,
+				color: req.body.color,
+				icon: req.body.icon,
+				image: req.body.image,
+			},
+			{ new: true }
+		);
+		if (!category) {
+			return res
+				.status(404)
+				.json({ success: false, message: "Category not found!" });
+		}
+		// Successfully updated
+		res.status(200).json({ success: true, category });
+	} catch (error) {
+		res.status(500).json({ success: false, message: error.message });
+	}
+});
+
+//router for add product by category
 router.post("/addcategory", async (req, res) => {
 	let category = new Category({
 		name: req.body.name,
